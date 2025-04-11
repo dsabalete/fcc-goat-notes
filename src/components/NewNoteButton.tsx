2cae7d3ff24add60052cf3494748f9c7dfe8c9e9
David Sabalete Rodríguez <dsabalete@gmail.com>
2025-04-12 00:52:20 +0200

    feat: add new note feature

--- src/components/NewNoteButton.tsx
+++ src/components/NewNoteButton.tsx
@@ -1,15 +1,51 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { v4 as uuidv4 } from "uuid";
 import { User } from "@supabase/supabase-js";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+import { createNoteAction } from "@/actions/notes";
+import { Button } from "./ui/button";
 
 type Props = {
   user: User | null;
 };
 
-async function NewNoteButton({ user }: Props) {
-  console.log(user?.email);
+function NewNoteButton({ user }: Props) {
+  const router = useRouter();
 
-  return <div>NewNoteButton</div>;
+  const [loading, setLoading] = useState(false);
+
+  const handleClickNewNoteButton = async () => {
+    if (!user) {
+      router.push("/login");
+    } else {
+      setLoading(true);
+
+      const uuid = uuidv4();
+      await createNoteAction(uuid);
+      router.push(`/?noteId=${uuid}`);
+
+      toast.success("New note created", {
+        description: "You can start writing your note now.",
+      });
+
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Button
+      onClick={handleClickNewNoteButton}
+      variant="secondary"
+      className="w-24"
+      disabled={loading}
+    >
+      {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "New Note"}
+    </Button>
+  );
 }
 
 export default NewNoteButton;
